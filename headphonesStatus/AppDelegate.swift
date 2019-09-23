ae4b5205603cfd7f2517e384e5d9da38d765b95a
Julien Bloit <julien.bloit@gmail.com>
2019-09-23 09:05:30 +0200

    Initial Commit

---
+++ headphonesStatus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  headphonesStatus
+//
+//  Created by julien@macmini on 23/09/2019.
+//  Copyright © 2019 jbloit. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ headphonesStatus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  headphonesStatus
+//
+//  Created by julien@macmini on 23/09/2019.
+//  Copyright © 2019 jbloit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
